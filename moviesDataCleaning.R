17c3834521dbf99400dc8015ce48d22c6258a95a
Sanjana <sanjanabandaru0@gmail.com>
2024-12-06 15:56:24 -0500

    Update moviesDataCleaning.R
    Remove Adult Movies
    Remove Movies with Missing Data
    Add Filters on Runtime, Budget, and the Number of Votes

--- moviesDataCleaning.R
+++ moviesDataCleaning.R
@@ -5,17 +5,22 @@ moviesRaw <- read_csv(
 )
 
 moviesCleaned <- moviesRaw %>%
-  dplyr:: select(-id, -vote_average, -vote_count, -overview, -adult, 
+  dplyr:: select(-id, -vote_average, -vote_count, -overview, 
   -backdrop_path, -homepage, -tconst, -poster_path, -tagline, -keywords, 
   -directors, -writers, -cast, -original_title, -popularity
   ) %>%
+  filter(adult == FALSE) %>%
   filter(grepl('English', spoken_languages)) %>%
-  dplyr:: select(-original_language, -spoken_languages) %>%
+  filter(original_language == "en") %>%
+  dplyr:: select(-original_language, -spoken_languages, -adult) %>%
   filter(!grepl('19', release_date)) %>%
   filter(status == "Released") %>%
-  filter(runtime != "0") %>%
-  filter(revenue != "0") %>%
-  filter(budget != "0") %>%
-  filter(!duplicated(title))
+  filter(revenue > 1) %>%
+  filter(runtime >= 30 ) %>%
+  filter(budget >=  1000) %>%
+  filter(numVotes >=  1000) %>%
+  filter(!duplicated(title)) %>%
+  filter(!grepl('UFC', title)) %>%
+  drop_na()
 
 View(moviesCleaned)
